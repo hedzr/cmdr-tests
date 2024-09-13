0d950f389b85f0bd053f4ca17bd08720c3eef5c6
hz <hedzrz@gmail.com>
2024-09-13 19:44:13 +0800

    use Cmd/Flg and deprecated AddCmd/AddFlg

--- examples/demo/demo.go
+++ examples/demo/demo.go
@@ -34,53 +34,50 @@ func prepareApp() (app cli.App) {
 	app = cmdr.New().
 		Info("demo-app", "0.3.1").
 		Author("hedzr")
-	app.AddFlg(func(b cli.FlagBuilder) {
-		b.Titles("no-default").
-			Description("disable force default action").
-			OnMatched(func(f *cli.Flag, position int, hitState *cli.MatchState) (err error) {
-				app.Store().Set("app.force-default-action", false)
-				return
-			})
-	})
-	app.AddCmd(func(b cli.CommandBuilder) {
-		b.Titles("jump").
-			Description("jump command").
-			Examples(`jump example`).
-			Deprecated(`jump is a demo command`).
-			Hidden(false)
+	app.Flg("no-default").
+		Description("disable force default action").
+		OnMatched(func(f *cli.Flag, position int, hitState *cli.MatchState) (err error) {
+			app.Store().Set("app.force-default-action", false)
+			return
+		}).
+		Build()
 
-		b.AddCmd(func(b cli.CommandBuilder) {
-			b.Titles("to").
-				Description("to command").
-				Examples(``).
-				Deprecated(`v0.1.1`).
-				Hidden(false).
-				OnAction(func(cmd *cli.Command, args []string) (err error) {
-					app.Store().Set("app.demo.working", dir.GetCurrentDir())
-					println()
-					println(dir.GetCurrentDir())
-					println()
-					println(app.Store().Dump())
-					return // handling command action here
-				})
-			b.AddFlg(func(b cli.FlagBuilder) {
-				b.Default(false).
-					Titles("full", "f").
-					Description("full command").
-					Build()
-			})
+	b := app.Cmd("jump").
+		Description("jump command").
+		Examples(`jump example`).
+		Deprecated(`jump is a demo command`).
+		Hidden(false)
+
+	b1 := b.Cmd("to").
+		Description("to command").
+		Examples(``).
+		Deprecated(`v0.1.1`).
+		Hidden(false).
+		OnAction(func(cmd *cli.Command, args []string) (err error) {
+			app.Store().Set("app.demo.working", dir.GetCurrentDir())
+			println()
+			println(dir.GetCurrentDir())
+			println()
+			println(app.Store().Dump())
+			return // handling command action here
 		})
-	})
+	b1.Flg("full", "f").
+		Default(false).
+		Description("full command").
+		Build()
+	b1.Build()
 
-	app.AddFlg(func(b cli.FlagBuilder) {
-		b.Titles("dry-run", "n").
-			Default(false).
-			Description("run all but without committing")
-	})
+	b.Build()
+
+	app.Flg("dry-run", "n").
+		Default(false).
+		Description("run all but without committing").
+		Build()
 
 	app.Flg("wet-run", "w").
 		Default(false).
 		Description("run all but with committing").
 		Build() // no matter even if you're adding the duplicated one.
+
 	return
 }
--- testdata/rootcmd.go
+++ testdata/rootcmd.go
@@ -1,10 +1,11 @@
 package testdata
 
 import (
+	"github.com/hedzr/cmdr-tests/examples"
+
 	"github.com/hedzr/cmdr/v2/builder"
 	"github.com/hedzr/cmdr/v2/cli"
 	"github.com/hedzr/cmdr/v2/cli/worker"
-	"github.com/hedzr/cmdr/v2/examples"
 )
 
 func BuildCommands(helpScreen bool) *cli.RootCommand {
@@ -64,91 +65,85 @@ func buildDemoApp() (app cli.App) {
 		Info("demo-app", "0.3.1").
 		Author("hedzr")
 
-	app.AddCmd(func(b cli.CommandBuilder) {
-		b.Titles("jump").
-			Description("jump command").
-			Examples(`jump example`).
-			Deprecated(`jump is a demo command`).
-			Hidden(false).
-			AddCmd(func(b cli.CommandBuilder) {
-				b.Titles("to").
-					Description("to command").
-					Examples(``).
-					Deprecated(``).
-					Hidden(false).
-					OnAction(func(cmd *cli.Command, args []string) (err error) {
-						return // handling command action here
-					})
+	b := app.Cmd("jump").
+		Description("jump command").
+		Examples(`jump example`).
+		Deprecated(`jump is a demo command`).
+		Hidden(false)
+	b1 := b.Cmd("to").
+		Description("to command").
+		Examples(``).
+		Deprecated(``).
+		Hidden(false).
+		OnAction(func(cmd *cli.Command, args []string) (err error) {
+			return // handling command action here
+		})
 
-				b.AddFlg(func(b cli.FlagBuilder) {
-					b.Default(false).
-						Titles("full", "f").
-						Description("full command")
-				})
-			})
-	}).AddFlg(func(b cli.FlagBuilder) {
-		b.Titles("dry-run", "n").
-			Default(false).
-			Description("run all but without committing")
-	})
+	b1.Flg("full", "f").
+		Default(false).
+		Description("full command").
+		Build()
+	b1.Build()
+
+	b.Build()
+
+	app.Flg("dry-run", "n").
+		Default(false).
+		Description("run all but without committing").
+		Build()
 
 	app.Flg("wet-run", "w").
 		Default(false).
 		Description("run all but with committing").
 		Build() // no matter even if you're adding the duplicated one.
 
-	app.AddCmd(func(b cli.CommandBuilder) {
-		b.Titles("consul", "c").
-			Description("command set for consul operations")
+	b = app.Cmd("consul", "c").
+		Description("command set for consul operations")
 
-		b.Flg("data-center", "dc", "datacenter").
-			// Description("set data-center").
-			Default("dc-1").
-			Build()
-	})
+	b.Flg("data-center", "dc", "datacenter").
+		// Description("set data-center").
+		Default("dc-1").
+		Build()
 
-	app.AddCmd(func(b cli.CommandBuilder) {
-		examples.AttachServerCommand(b)
-	})
+	b.Build()
+
+	examples.AttachServerCommand(app.NewCommandBuilder("server"))
 
 	// app.AddCmd(func(b cli.CommandBuilder) {
 	// 	examples.AttachKvCommand(b)
 	// })
 
-	app.AddCmd(func(b cli.CommandBuilder) {
-		examples.AttachMsCommand(b)
-	})
+	examples.AttachMsCommand(app.NewCommandBuilder("ms"))
 
 	// app.AddCmd(func(b cli.CommandBuilder) {
 	// 	examples.AttachMoreCommandsForTest(b)
 	// })
 
-	app.AddCmd(func(b cli.CommandBuilder) {
-		b.Titles("display", "da").
-			Description("command set for display adapter operations")
+	b = app.Cmd("display", "da").
+		Description("command set for display adapter operations")
 
-		b1 := b.Cmd("voodoo", "vd").
-			Description("command set for voodoo operations")
-		b1.Flg("data-center", "dc", "datacenter").
-			Default("dc-1").
-			Build()
-		b1.Build()
+	b1 = b.Cmd("voodoo", "vd").
+		Description("command set for voodoo operations")
+	b1.Flg("data-center", "dc", "datacenter").
+		Default("dc-1").
+		Build()
+	b1.Build()
 
-		b2 := b.Cmd("nvidia", "nv").
-			Description("command set for nvidia operations")
-		b2.Flg("data-center", "dc", "datacenter").
-			Default("dc-1").
-			Build()
-		b2.Build()
+	b2 := b.Cmd("nvidia", "nv").
+		Description("command set for nvidia operations")
+	b2.Flg("data-center", "dc", "datacenter").
+		Default("dc-1").
+		Build()
+	b2.Build()
 
-		b.AddCmd(func(b cli.CommandBuilder) {
-			b.Titles("amd", "amd").
-				Description("command set for AMD operations")
-			b.Flg("data-center", "dc", "datacenter").
-				Default("dc-1").
-				Build()
-		})
-	})
+	b3 := b.Cmd("amd", "amd").
+		Description("command set for AMD operations")
+	b3.Flg("data-center", "dc", "datacenter").
+		Default("dc-1").
+		Build()
+	b3.Build()
+
+	b.Build()
 
 	return
 }
