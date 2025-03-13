c1c93990ba2943f0068da86a01724b138c416cdf
hz <hedzrz@gmail.com>
2025-03-13 11:12:21 +0800

    update demo-app

--- examples/demo/demo.go
+++ examples/demo/demo.go
@@ -2,143 +2,47 @@ package main
 
 import (
 	"context"
-	"io"
 	"os"
 
-	"gopkg.in/hedzr/errors.v3"
-
-	"github.com/hedzr/cmdr-loaders/local"
-	logz "github.com/hedzr/logg/slog"
-	"github.com/hedzr/store"
-
+	loaders "github.com/hedzr/cmdr-loaders"
+	"github.com/hedzr/cmdr-tests/examples/demo/cmd"
 	"github.com/hedzr/cmdr/v2"
 	"github.com/hedzr/cmdr/v2/cli"
-	"github.com/hedzr/cmdr/v2/pkg/dir"
+	"github.com/hedzr/cmdr/v2/examples"
+	"github.com/hedzr/cmdr/v2/examples/devmode"
+	logz "github.com/hedzr/logg/slog"
 )
 
 func main() {
 	ctx := context.Background()
-	app := prepareApp(
-		cmdr.WithStore(store.New()),
-		cmdr.WithExternalLoaders(
-			local.NewConfigFileLoader(),
-			local.NewEnvVarLoader(),
-		),
-
-		cmdr.WithTasksBeforeRun(func(ctx context.Context, cmd cli.Cmd, runner cli.Runner, extras ...any) (err error) {
-			logz.DebugContext(ctx, "command running...", "cmd", cmd, "runner", runner, "extras", extras)
-			return
-		}), // cmdr.WithTasksBeforeParse(), cmdr.WithTasksBeforeRun(), cmdr.WithTasksAfterRun
-
-		// true for debug in developing time, it'll disable onAction on each Cmd.
-		// for productive mode, comment this line.
-		// The envvars FORCE_DEFAULT_ACTION & FORCE_RUN can override this.
-		cmdr.WithForceDefaultAction(true),
-
-		// cmdr.WithSortInHelpScreen(true),       // default it's false
-		// cmdr.WithDontGroupInHelpScreen(false), // default it's false
-		// cmdr.WithAutoEnvBindings(true),
-	)
-
-	// // simple run the parser of app and trigger the matched command's action
-	// _ = app.Run(
-	// 	cmdr.WithForceDefaultAction(false), // true for debug in developing time
-	// )
-
+	app := prepareApp(cmd.Commands...) // define your own commands implementations with cmd/*.go
 	if err := app.Run(ctx); err != nil {
 		logz.ErrorContext(ctx, "Application Error:", "err", err) // stacktrace if in debug mode/build
 		os.Exit(app.SuggestRetCode())
+	} else if rc := app.SuggestRetCode(); rc != 0 {
+		os.Exit(rc)
 	}
 }
 
-func prepareApp(opts ...cli.Opt) (app cli.App) {
-	app = cmdr.New(opts...).
-		Info(AppNameExample, "0.3.1").
-		Author("The Example Authors") // .Description(``).Header(``).Footer(``)
-
-	// another way to disable `cmdr.WithForceDefaultAction(true)` is using
-	// env-var FORCE_RUN=1 (builtin already).
-	app.Flg("no-default").
-		Description("disable force default action").
-		OnMatched(func(f *cli.Flag, position int, hitState *cli.MatchState) (err error) {
-			if b, ok := hitState.Value.(bool); ok {
-				f.Set().Set("app.force-default-action", b) // disable/enable the final state about 'force default action'
-			}
-			return
-		}).
+func prepareApp(commands ...cli.CmdAdder) (app cli.App) {
+	return loaders.Create(
+		appName, version, author, desc,
+		append([]cli.Opt{
+			cmdr.WithAutoEnvBindings(true),  // default it's false
+			cmdr.WithSortInHelpScreen(true), // default it's false
+			// cmdr.WithDontGroupInHelpScreen(false), // default it's false
+			// cmdr.WithForceDefaultAction(false),
+		})...,
+	).
+		// importing devmode package and run its init():
+		With(func(app cli.App) { logz.Debug("in dev mode?", "mode", devmode.InDevelopmentMode()) }).
+		WithBuilders(
+			examples.AddHeadLikeFlagWithoutCmd, // add a `--line` option, feel free to remove it.
+			examples.AddToggleGroupFlags,       //
+			examples.AddTypedFlags,             //
+			examples.AddKilobytesFlag,          //
+			examples.AddValidArgsFlag,          //
+		).
+		WithAdders(commands...).
 		Build()
-
-	app.Cmd("jump").
-		Description("jump command").
-		Examples(`jump example`). // {{.AppName}}, {{.AppVersion}}, {{.DadCommands}}, {{.Commands}}, ...
-		Deprecated(`v1.1.0`).
-		// Group(cli.UnsortedGroup).
-		Hidden(false, false).
-		// OnEvaluateSubCommands(onEvalJumpSubCommands).
-		With(func(b cli.CommandBuilder) {
-			b.Cmd("to").
-				Description("to command").
-				Examples(``).
-				Deprecated(`v0.1.1`).
-				// Group(cli.UnsortedGroup).
-				Hidden(false).
-				OnAction(func(ctx context.Context, cmd cli.Cmd, args []string) (err error) {
-					// cmd.Set() == cmdr.Store(), cmd.Store() == cmdr.Store()
-					cmd.Set().Set("app.demo.working", dir.GetCurrentDir())
-					println()
-					println(cmd.Set().WithPrefix("app.demo").MustString("working"))
-
-					cs := cmdr.Store().WithPrefix("jump.to")
-					if cs.MustBool("full") {
-						println()
-						println(cmd.Set().Dump())
-					}
-					cs2 := cmd.Store()
-					if cs2.MustBool("full") != cs.MustBool("full") {
-						logz.Panic("a bug found")
-					}
-					app.SetSuggestRetCode(1) // ret code must be in 0-255
-					return                   // handling command action here
-				}).
-				With(func(b cli.CommandBuilder) {
-					b.Flg("full", "f").
-						Default(false).
-						Description("full command").
-						// Group(cli.UnsortedGroup).
-						Build()
-				})
-		})
-
-	app.Flg("dry-run", "n").
-		Default(false).
-		Description("run all but without committing").
-		Group(cli.UnsortedGroup).
-		Build()
-
-	app.Flg("wet-run", "w").
-		Default(false).
-		Description("run all but with committing").
-		Build() // no matter even if you're adding the duplicated one.
-
-	app.Cmd("wrong").
-		Description("a wrong command to return error for testing").
-		// cmdline `FORCE_RUN=1 go run ./tiny wrong -d 8s` to verify this command to see the returned application error.
-		OnAction(func(ctx context.Context, cmd cli.Cmd, args []string) (err error) {
-			dur := cmd.Store().MustDuration("duration")
-			println("the duration is:", dur.String())
-
-			ec := errors.New()
-			defer ec.Defer(&err) // store the collected errors in native err and return it
-			ec.Attach(io.ErrClosedPipe, errors.New("something's wrong"), os.ErrPermission)
-			// see the application error by running `go run ./tiny/tiny/main.go wrong`.
-			return
-		}).
-		With(func(b cli.CommandBuilder) {
-			b.Flg("duration", "d").
-				Default("5s").
-				Description("a duration var").
-				Build()
-		})
-
-	return
 }
