f5cac6b6271b283ba561e530bd15a4020a520c43
hz <hedzrz@gmail.com>
2025-03-20 12:24:22 +0800

    update `blueprint` app

--- examples/blueprint/cmd/all.go
+++ examples/blueprint/cmd/all.go
@@ -2,12 +2,11 @@ package cmd
 
 import (
 	"github.com/hedzr/cmdr/v2/cli"
-	"github.com/hedzr/cmdr/v2/examples/cmd"
 )
 
 var Commands = append(
 	[]cli.CmdAdder{
 		sndx{},
 	},
-	cmd.Commands...,
+	// cmd.Commands...,
 )
--- examples/blueprint/main.go
+++ examples/blueprint/main.go
@@ -5,10 +5,9 @@ import (
 	"os"
 
 	loaders "github.com/hedzr/cmdr-loaders"
+	"github.com/hedzr/cmdr-tests/examples/blueprint/cmd"
 	"github.com/hedzr/cmdr/v2"
 	"github.com/hedzr/cmdr/v2/cli"
-	"github.com/hedzr/cmdr/v2/examples"
-	"github.com/hedzr/cmdr/v2/examples/blueprint/cmd"
 	"github.com/hedzr/cmdr/v2/examples/devmode"
 	"github.com/hedzr/cmdr/v2/pkg/logz"
 )
@@ -35,32 +34,31 @@ func main() {
 func prepareApp(commands ...cli.CmdAdder) cli.App {
 	return loaders.Create(
 		appName, version, author, desc,
-		append([]cli.Opt{
-			cmdr.WithAutoEnvBindings(true),  // default it's false
-			cmdr.WithSortInHelpScreen(true), // default it's false
-			// cmdr.WithDontGroupInHelpScreen(false), // default it's false
 
-			// can be used for debugging:
-			//
-			// cmdr.WithTasksBeforeRun(func(ctx context.Context, cmd cli.Cmd, runner cli.Runner, extras ...any) (err error) {
-			// 	logz.DebugContext(ctx, "command running...", "cmd", cmd, "runner", runner, "extras", extras)
-			// 	return
-			// }), // cmdr.WithTasksBeforeParse(), cmdr.WithTasksBeforeRun(), cmdr.WithTasksAfterRun
+		cmdr.WithAutoEnvBindings(true),  // default it's false
+		cmdr.WithSortInHelpScreen(true), // default it's false
+		// cmdr.WithDontGroupInHelpScreen(false), // default it's false
 
-			// true for debug in developing time, it'll disable onAction on each Cmd.
-			// for productive mode, comment this line.
-			// The envvars FORCE_DEFAULT_ACTION & FORCE_RUN can override this.
-			// cmdr.WithForceDefaultAction(false),
-		})...,
+		// can be used for debugging:
+		//
+		// cmdr.WithTasksBeforeRun(func(ctx context.Context, cmd cli.Cmd, runner cli.Runner, extras ...any) (err error) {
+		// 	logz.DebugContext(ctx, "command running...", "cmd", cmd, "runner", runner, "extras", extras)
+		// 	return
+		// }), // cmdr.WithTasksBeforeParse(), cmdr.WithTasksBeforeRun(), cmdr.WithTasksAfterRun
+
+		// true for debug in developing time, it'll disable onAction on each Cmd.
+		// for productive mode, comment this line.
+		// The envvars FORCE_DEFAULT_ACTION & FORCE_RUN can override this.
+		// cmdr.WithForceDefaultAction(false),
 	).
 		// importing devmode package and run its init():
 		With(func(app cli.App) { logz.Debug("in dev mode?", "mode", devmode.InDevelopmentMode()) }).
 		WithBuilders(
-			examples.AddHeadLikeFlagWithoutCmd, // add a `--line` option, feel free to remove it.
-			examples.AddToggleGroupFlags,       //
-			examples.AddTypedFlags,             //
-			examples.AddKilobytesFlag,          //
-			examples.AddValidArgsFlag,          //
+		// examples.AddHeadLikeFlagWithoutCmd, // add a `--line` option, feel free to remove it.
+		// examples.AddToggleGroupFlags,       //
+		// examples.AddTypedFlags,    //
+		// examples.AddKilobytesFlag, //
+		// examples.AddValidArgsFlag,          //
 		).
 		WithAdders(commands...).
 		Build()
